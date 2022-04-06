e5549c9448acdef5a2fcbef481d3c32045a7cda1
ghost <arias0723@gmail.com>
2022-04-06 17:49:17 -0300

    Initial awesome cfg

---
+++ README.md
@@ -0,0 +1,20 @@
+# BSPWM Rice
+**BSPWM Rice**
+
+
+## To Install manually
+
+    git clone https://github.com/arias0723/bspwm.git
+
+    cd bspwm
+
+    bash bspwm-install.sh
+   
+
+## Issues
+
+    For alacritty
+        Add $LIBGL_ALWAYS_SOFTWARE=true to /etc/environment
+    
+    GLX and lagging DE
+        Use picom.xrender config file
---
+++ awesomewm-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Init
+CURRENT_USER=$(cat /etc/passwd | grep "/home" |cut -d: -f1 |head -1)
+ARCH_PACKAGES=`grep -vE "^#" arch-packages.txt`
+AUR_PACKAGES=`grep -vE "^#" aur-packages.txt`
+mkdir -p ~/.config
+mkdir -p ~/.local/share/fonts
+mkdir ~/Pictures
+
+# Install base packages
+sudo pacman -Syu --needed --noconfirm $ARCH_PACKAGES
+yay -S --nodiffmenu --noremovemake --answerclean All --noconfirm $AUR_PACKAGES
+
+# WM cfg
+cp -R resources/.config/. ~/.config/
+cp -R resources/.screenlayout ~/
+cp -R resources/.Xresources.d ~/
+cp -R resources/wallpapers/. ~/Pictures
+cp -R resources/fonts/. ~/.local/share/fonts
+#cp resources/.Xresources ~/
+#cp resources/.xinitrc ~/
+#cp resources/.gtkrc-2.0 ~/
+chmod -R +x ~/.screenlayout
+#chmod +x ~/.xinitrc
+
+# ZSH cfg
+#cp -R resources/zsh/theme/. ~/
+#sudo cp -R resources/zsh/theme/. /root
+#sudo cp -R resources/zsh/plugins/. ~/ /usr/share
+#sudo ln -s -f ~/.zshrc /root/.zshrc
+#sudo usermod --shell /usr/bin/zsh ghost
+#sudo usermod --shell /usr/bin/zsh root
+
+# Misc
+sudo systemctl enable NetworkManager.service
+sudo fc-cache -f -v
+

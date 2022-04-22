7b6af39e3238eefdf86894cb4a1d46c5f6d6864b
gh0st <arias0723@gmail.com>
2022-04-22 15:43:15 -0300

    Added fs linker

--- link-to-fs.sh
+++ link-to-fs.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# dirs=("cat" "dog" "mouse" "frog")
-dirs=("./resources/.config/awesome")
+FS_CONFIG_DIR="$HOME/.config"
+GIT_CONFIG_DIR="./resources/.config"
 
-for str in ${dirs[@]}; do
-	config_files=$(find $str -type f)
-	for cfg_file in $config_files; do
-		echo $cfg_file
+dir_names=("awesome")
+
+for dir_name in ${dir_names[@]}; do
+	config_dir="$FS_CONFIG_DIR/$dir_name"
+	config_files_in_dir=$(find $config_dir -type f -exec realpath --relative-to $FS_CONFIG_DIR {} \;)
+
+	# hard link to 'real' cfg files
+	for cfg_file in $config_files_in_dir; do
+		fs_cfg_file="$FS_CONFIG_DIR/$cfg_file"		
+		git_cfg_file="$GIT_CONFIG_DIR/$cfg_file"
+		#echo $fs_cfg_file
+		#echo $git_cfg_file
+		ln -f $fs_cfg_file $git_cfg_file
 	done
 done
-
-ln -f ~/.config/awesome/rc.lua ./resources/.config/awesome/rc.lua
